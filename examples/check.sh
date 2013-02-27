91b05a72d56240cb5d7723e1edc05296406b9e4e
francis.zw@gmail.com <francis.zw@gmail.com@b1d3c554-91dd-31f3-c319-5d57912e143a>
2013-02-27 02:07:50 +0000

    about to change instr data structure

--- examples/check.sh
+++ examples/check.sh
@@ -5,7 +5,7 @@ C_SUBSET_COMPILER=../../csc/csc
 THREE_ADDR_TO_C_TRANSLATOR=../../3addr-converter/convert.py
 
 for PROGRAM in collatz.c gcd.c hanoifibfac.c loop.c mmm.c prime.c \
-    regslarge.c struct.c sort.c sieve.c
+    regslarge.c struct.c sort.c sieve.c test.c
 do
 
   BASENAME=`basename $PROGRAM .c`
