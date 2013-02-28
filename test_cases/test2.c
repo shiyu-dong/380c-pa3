e8976918c71a800405d9eeb565374ae2b9bfd812
dongshiyu1989@gmail.com <dongshiyu1989@gmail.com@b1d3c554-91dd-31f3-c319-5d57912e143a>
2013-02-28 03:34:54 +0000

    add test case test2.c and test2alllive.c

---
+++ test_cases/test2.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#define WriteLine() printf("\n");
+#define WriteLong(x) printf(" %lld", x);
+#define ReadLong(a) if (fscanf(stdin, "%lld", &a) != 1) a = 0;
+#define long long long
+
+
+const long m = 4;
+const long n = 3;
+long m1[m][n], m2[m][n];//should be always live
+
+struct A {
+  long x, y;
+} globala;
+
+void main()
+{
+  long m3[m][n];//should be always dead
+  long i, j, k;
+  struct A locala;
+
+  i = 0;
+  //test global and local array;
+  while (i < m) {
+    j = 0;
+    while (j < n) {
+      m1[i][j] = i+j*2;
+      m2[i][j] = -m1[i][j];
+      m3[i][j] = i*2+j;
+      j = j + 1;
+    }
+  }
+
+  //test global struct and local struct
+
+  k = 0;
+  while (k < 10) {
+    //dead struct
+    locala.x = k;
+    locala.y = -k;
+    //live struct
+    globala.x = k;
+    globala.y = -k;
+
+    k = k + 1;
+  }
+  
+  //test empty block loop
+  while (k > 0) {
+    while (k > 1) {
+      while (k > 2) {
+        if (k < 3) {}
+          //do nothing
+        else {}
+          //do nothing
+      }
+    }
+  }
+
+}
